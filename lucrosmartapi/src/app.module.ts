15a9c595a5da18639bba147f32a711e20b942c21
Ricardo Frederico Thiesen <ricardothiesen@outlook.com>
2024-12-07 18:56:05 -0300

    feat: database config, created new resources

--- lucrosmartapi/src/app.module.ts
+++ lucrosmartapi/src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuppliesModule } from './supplies/supplies.module';
+import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module';
+import { PricingModule } from './pricing/pricing.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SuppliesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    SuppliesModule,
+    ProductsModule,
+    SalesModule,
+    PricingModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ lucrosmartapi/src/config.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URI;
+
+export default { mongoUri };
