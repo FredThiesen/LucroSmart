1acb46847fc850ab3e4f63b865fcfeaf4ef0664f
Ricardo Frederico Thiesen <ricardothiesen@outlook.com>
2024-12-08 02:11:13 -0300

    fix: fix swagger

--- lucrosmartapi/src/supplies/supplies.service.ts
+++ lucrosmartapi/src/supplies/supplies.service.ts
@@ -3,31 +3,27 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateSupplyDto } from './dto/create-supply.dto';
 import { UpdateSupplyDto } from './dto/update-supply.dto';
-import { Supply } from './supplies.schema'; // Supondo que você tenha criado um schema chamado Supply
+import { Supply } from './supplies.schema';
 
 @Injectable()
 export class SuppliesService {
   constructor(
-    @InjectModel(Supply.name) private readonly supplyModel: Model<Supply>, // Injeta o modelo do Supply
+    @InjectModel(Supply.name) private readonly supplyModel: Model<Supply>,
   ) {}
 
-  // Cria um novo supply
   async create(createSupplyDto: CreateSupplyDto): Promise<Supply> {
     const newSupply = new this.supplyModel(createSupplyDto);
-    return newSupply.save(); // Salva o novo supply no banco de dados
+    return newSupply.save();
   }
 
-  // Retorna todos os supplies
   async findAll(): Promise<Supply[]> {
-    return this.supplyModel.find().exec(); // Encontra todos os supplies
+    return this.supplyModel.find().exec();
   }
 
-  // Retorna um supply específico pelo ID
   async findOne(id: string): Promise<Supply | null> {
-    return this.supplyModel.findById(id).exec(); // Encontra o supply pelo ID
+    return this.supplyModel.findById(id).exec();
   }
 
-  // Atualiza um supply existente
   async update(
     id: string,
     updateSupplyDto: UpdateSupplyDto,
@@ -35,11 +31,9 @@ export class SuppliesService {
     return this.supplyModel
       .findByIdAndUpdate(id, updateSupplyDto, { new: true })
       .exec();
-    // Atualiza e retorna o supply atualizado
   }
 
-  // Remove um supply pelo ID
   async remove(id: string): Promise<Supply | null> {
-    return this.supplyModel.findByIdAndDelete(id).exec(); // Deleta o supply pelo ID
+    return this.supplyModel.findByIdAndDelete(id).exec();
   }
 }
